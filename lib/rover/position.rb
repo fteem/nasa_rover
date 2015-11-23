989cf579f69ad1266bd92dd3230037ec9b3f3ca0
Ile Eftimov <ileeftimov@gmail.com>
2015-11-23 15:30:45 +0100

    add position and movement

--- lib/rover/position.rb
+++ lib/rover/position.rb
@@ -5,5 +5,21 @@ module Rover
       @x = x
       @y = y
     end
+
+    def east
+      @x += 1
+    end
+
+    def west
+      @x -= 1
+    end
+
+    def north
+      @y += 1
+    end
+
+    def south
+      @y -= 1
+    end
   end
 end
