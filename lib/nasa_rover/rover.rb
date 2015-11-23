9fc1d9efa3cc62fbaa7b5e265897af0673b35c4e
Ile Eftimov <ileeftimov@gmail.com>
2015-11-23 15:53:30 +0100

    rover validates the direction

--- lib/nasa_rover/rover.rb
+++ lib/nasa_rover/rover.rb
@@ -1,4 +1,19 @@
 module NASARover
   class Rover
+
+    class InvalidDirection < StandardError; end
+
+    attr_reader :direction, :position
+
+    def initialize(position, facing_direction)
+      @direction = validate_direction!(facing_direction)
+      @position  = position
+    end
+
+    private
+    def validate_direction! direction
+      raise InvalidDirection.new(direction) unless %q(N S W E).include?(direction)
+      direction
+    end
   end
 end
--- test/rover_test.rb
+++ test/rover_test.rb
@@ -1,7 +1,28 @@
 require_relative 'test_helper'
 
 class RoverTest < Minitest::Test
-  def test_initialization
-    rover = NASARover::Rover.new(
+  def test_starting_position_x_coordiante
+    position = NASARover::Position.new(5, 6)
+    rover = NASARover::Rover.new(position, 'N')
+    assert_equal 5, rover.position.x
+  end
+
+  def test_starting_position_y_coordiante
+    position = NASARover::Position.new(5, 6)
+    rover = NASARover::Rover.new(position, 'N')
+    assert_equal 6, rover.position.y
+  end
+
+  def test_facing_direction
+    position = NASARover::Position.new(5, 6)
+    rover = NASARover::Rover.new(position, 'N')
+    assert_equal "N", rover.direction
+  end
+
+  def test_direction_validation_when_invalid_direction
+    position = NASARover::Position.new(5, 6)
+    assert_raises NASARover::Rover::InvalidDirection do
+      NASARover::Rover.new(position, 'H')
+    end
   end
 end
